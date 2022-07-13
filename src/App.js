90603f8039578dea2b939de00a75e1e37bbb3541
aldemir.dev <aldemirmorais29@gmail.com>
2022-07-12 23:00:08 -0300

    criando novo projeto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import {FiSearch} from 'react-icons/fi';
+import { DiGithubBadge } from "react-icons/di";
+import { FiLinkedin } from "react-icons/fi";
+import { FiInstagram } from "react-icons/fi";
+import { FiFacebook } from "react-icons/fi";
+import Logo from './mapa.svg';
+import './main.css';
+
+import api from './services/api';
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [cep, setCep] = useState({});
+
+  async function buscar() {
+    // 11015070/json/
+    
+    if(input === '') {
+      alert('Por favor, Digite um cep!')
+      return;
+    }
+    
+    try{
+      const response = await api.get(`${input}/json`);
+      setCep(response.data)
+      setInput("")
+
+    }catch{
+
+      alert('Ops! Cep Inexistente!')
+      setInput("")
+
+    }
+  }
+
   return (
-    <div className="App">
+  <div className="container">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <img src={Logo} className="App-logo" alt="logo" />
+        <a href="https://github.com/AldemirMoraisDeveloper" target="_blank">
+          <DiGithubBadge className="github"/>
+        </a>
+        <a href="https://www.linkedin.com/in/aldemir-morais-144ab21b9/" target="_blank">
+          <FiLinkedin className="linkedin"/>
+        </a>
+        <a href="https://www.instagram.com/aldemir.dev/" target="_blank">
+          <FiInstagram className="instagram"/>
+        </a>
+        <a href="https://www.facebook.com/Aldemir.FerreiraM/" target="_blank">
+          <FiFacebook className="facebook"/>
         </a>
       </header>
-    </div>
+      <h1 className="title">Buscar CEP</h1>
+
+      <div className="containerInput">
+         <input 
+         type="text"
+         placeholder= "Digite um cep..."   
+         value={input}
+         onChange={(event) => setInput(event.target.value) }    
+         />
+
+        <button className="btnSearch" onClick={buscar}>
+          <FiSearch size={25} color="#fff"/>
+        </button>
+      </div> 
+
+      {Object.keys(cep).length > 0 && (
+      <main className="main">
+        <h2>CEP: {cep.cep}</h2>
+
+        <span>{cep.logradouro}</span>
+        <span>Complemento: {cep.complemento}</span>
+        <span>{cep.bairro}</span>
+        <span>{cep.localidade} - {cep.uf}</span>
+      </main>
+      )}
+    
+  </div>
   );
 }
 
